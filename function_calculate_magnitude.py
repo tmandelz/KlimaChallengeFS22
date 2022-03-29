5d9b063a7c889ddb7c91cb0297dcd1a3dbec9cc9
swiggy123 <90757322+swiggy123@users.noreply.github.com>
2022-03-29 11:07:23 +0200

    Update function_calculate_magnitude.py

--- function_calculate_magnitude.py
+++ function_calculate_magnitude.py
@@ -43,3 +43,65 @@ def calculate_magnitude(df_country:pd.DataFrame,reference_period: str) -> pd.Dat
         saved_df["DAY"] = (start_time + timedelta(days = day_loop)).strftime('%m/%d')
         
         df_reference = pd.concat([df_reference, saved_df])
+    
+    
+
+    # Neues Datumsformat hinzufügen
+    df_values["month_day"] = df_values["DAY"].dt.strftime('%m/%d')
+    # Spalten umbennenen
+    reference_df = reference_df.rename(columns= {"DAY":"month_day","TEMPERATURE_MAX":"reference_temperature"})
+
+    # Werte löschen die kleiner sind als die Referenzwerte.
+    df_values_reference= pd.merge(df_values,reference_df,on=["GRID_NO","month_day"],how='left')
+    df_values_reference = df_values_reference[df_values_reference["TEMPERATURE_MAX"] > df_values_reference["reference_temperature"]].drop("month_day",axis=1)
+
+
+    # Maximum pro Jahr in der Referenzperiode ausrechnen
+    df_max_values = df_values[df_values["DAY"] < reference_period]
+    df_max_values.loc[:,"DAY"]= df_max_values.loc[:,"DAY"].dt.strftime('%y')
+    df_max_values = df_max_values.groupby(["GRID_NO","DAY"]).max()
+    # T30y25p und T30y75p ausrechnen
+    df_max_values = df_max_values.groupby("GRID_NO").quantile([0.25,0.75]).unstack()
+    df_max_values = df_max_values.loc[:,"TEMPERATURE_MAX"].reset_index()
+
+    # Magnitude ausrechnen
+    df_single_magnitudes = pd.merge(reference_df,df_max_values,on=["GRID_NO"],how='left')
+    df_single_magnitudes.loc[:,"magnitude"] = (df_single_magnitudes.loc[:,"TEMPERATURE_MAX"] - df_single_magnitudes.loc[:,0.25])/ (df_single_magnitudes.loc[:,0.75]-df_single_magnitudes.loc[:,0.25])
+    # Werte löschen die kleiner sind als T30y25p
+    df_single_magnitudes = df_single_magnitudes[df_single_magnitudes["TEMPERATURE_MAX"]>df_single_magnitudes[0.25]]
+
+
+    # Magnituden an aufeinanderfolgenden Tagen Summieren
+    df_single_magnitudes.sort_values(by= ["GRID_NO","DAY"])
+    df_single_magnitudes["Start_Date"] = df_single_magnitudes["DAY"]
+    df_single_magnitudes["End_Date"] = df_single_magnitudes["DAY"] + timedelta(days = 1)
+
+    
+    df_sum_magnitudes = pd.DataFrame()
+    new_safe = 0
+    # Durch alle Magnituden iterieren
+    for index, row in df_single_magnitudes.iterrows():
+        if new_safe == 1:
+            # Prüfen ob sie aufeinanderfolgend sind
+            if (row["GRID_NO"] == grid_no) & (end_date == row["Start_Date"]):
+                mangnitude += row["magnitude"]
+                end_date = row["End_Date"]    
+            else:
+                df_to_concat = pd.DataFrame({"GRID_NO":grid_no,"Start_Date" : start_date, "End_Date": end_date,"sum_magnitude": mangnitude},index=[0])
+                df_sum_magnitudes = pd.concat([df_sum_magnitudes,df_to_concat])
+                new_safe = 0
+
+        if new_safe == 0:
+            new_safe = 1
+            grid_no = row["GRID_NO"]
+            start_date = row["Start_Date"]
+            end_date = row["End_Date"]
+            mangnitude = row["magnitude"]
+    
+    df_sum_magnitudes = df_sum_magnitudes.reset_index(drop=True)
+
+    # Longtitude Latitude und Altitude mergen
+    df_output = pd.merge(df_sum_magnitudes,df_normalised,on=["GRID_NO"],how='left')
+
+    return df_output
+    
