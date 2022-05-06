6d6cd37a5ba8bd3529af147c77f73de83ca9f97a
patschue <84413011+patschue@users.noreply.github.com>
2022-05-06 17:46:28 +0200

    Update Plotvier

--- Plots/Plot4/Plotvier.py
+++ Plots/Plot4/Plotvier.py
@@ -9,6 +9,7 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 import os
+import statsmodels
 
 grid = 95097
 year = 2018
@@ -25,9 +26,9 @@ mag = pd.read_csv(magnitudePath, sep=";")
 threshold = pd.read_csv(threshholdPath, sep = ";")
 data = pd.read_csv(LuxembourgPath, sep =";")
 
-mag = mag.loc[mag['GRID_NO']==grid]
-threshold = threshold.loc[threshold['GRID_NO']==grid]
-data = data.loc[data['GRID_NO']==grid]
+# mag = mag.loc[mag['GRID_NO']==grid]
+# threshold = threshold.loc[threshold['GRID_NO']==grid]
+# data = data.loc[data['GRID_NO']==grid]
 
 # print(mag.dtypes)
 # print(threshold.dtypes)
@@ -48,8 +49,10 @@ magni = magni.loc[magni['Count'] >= 3]
 # magni["End"] = magni["Start"] + magni["Count"]
 magni = magni.reset_index(drop=True)
 magni["Date"] = pd.to_datetime(magni["Start"], format='%Y%m%d')
-magniperyear = magni.groupby([magni["Date"].dt.year])["Sum"].agg("sum")
-
+magni["Year"] = magni['Date'].dt.year
+print(magni)
+magniperyear = magni.groupby([magni["Date"].dt.year])["Sum", "Count"].agg("sum")
+print(magniperyear)
 
 # ####
 
@@ -63,46 +66,88 @@ magniperyear = magni.groupby([magni["Date"].dt.year])["Sum"].agg("sum")
 # merged = pd.merge(data, threshold, on = "NoDay")
 # merged = pd.merge(merged, mag, how = "left", on = "NoDay")
 
-# # fig = px.line(merged, x = "NoDay", y = ["reference_temperature_x", "TEMPERATURE_MAX_x"])
-# # fig.add_bar(x = "NoDay", y = "magnitude")
-
-fig = go.Figure()
-
-fig.add_trace(
-    go.Scatter(
-        x=magniperyear.index,
-        y=magniperyear,
-        mode='markers',
-        # line_color = "blue",
-        name = "Sum per Year"
-    ))
+# fig = go.Figure()
 
 # fig.add_trace(
 #     go.Scatter(
-#         x=merged["NoDay"],
-#         y=merged["TEMPERATURE_MAX_x"],
-#         line_color = "red",
-#         name = "Max Temperature"
+#         x=magniperyear.index,
+#         y=magniperyear,
+#         mode='markers',
+#         # line_color = "blue",
+#         name = "Sum per Year"
 #     ))
 
-# # fig.add_trace(
-# #     go.Bar(
-# #         x=magni["Start"],
-# #         y=magni["Count"],
-# #         marker_color = "orange"
-# #     ))
 
-# # fig.add_vrect(x0=magni.loc[0,"Start"], x1=magni.loc[0, "End"], 
-# #                annotation_text="Anzahl Tage", annotation_position="top left",
-# #                annotation=dict(font_size=15, font_family="Arial"),
-# #               fillcolor="orange", opacity=0.25, line_width=0)
+# # fig = px.line(merged, x = "NoDay", y = ["reference_temperature_x", "TEMPERATURE_MAX_x"])
+# # fig.add_bar(x = "NoDay", y = "magnitude")
 
-# for x in range(len(magni)):
-#     c = magni.loc[x,"Count"]
-#     fig.add_vrect(x0=magni.loc[x,"Start"], x1=magni.loc[x, "End"], 
-#                 annotation_text="Anzahl Tage: %s" %c, annotation_position="bottom",
-#                 annotation=dict(font_size=15, font_family="Arial", textangle=-90),
-#               fillcolor="orange", opacity=0.25, line_width=0),
+# fig = go.Figure()
+
+# fig.add_trace(
+#     go.Scatter(
+#         x=magniperyear.index,
+#         y=magniperyear,
+#         mode='markers',
+#         # line_color = "blue",
+#         name = "Sum per Year"
+#     ))
+
+# fig = px.imshow(magniperyear, color_continuous_scale='RdBu_r', origin='lower')
+# fig = px.heatmap(magniperyear, x=magniperyear.index, y="Sum", nbinsx=80, nbinsy=20, color_continuous_scale="Viridis")
+
+# fig = px.scatter(
+#     magni,
+#     x="Year",
+    # y="Sum",
+    # trendline="lowess"
+    # mode='markers',
+    # line_color = "blue",
+    # name = "Sum per Year"
+    # )
+
+# fig = px.scatter(
+#     magniperyear,
+#     x=magniperyear.index,
+#     y="Sum",
+#     color='Count',
+#     color_continuous_scale=px.colors.sequential.Oranges
+#     )
+
+# This:
+# fig = px.scatter(
+#     magniperyear,
+#     x=magniperyear.index,
+#     y="Sum",
+#     trendline="rolling",
+#     trendline_options=dict(window=10)
+#     )
+# fig.update_traces(marker={'size': 5, 'color': 'green'})
+
+fig = px.bar(
+    magniperyear,
+    x=magniperyear.index,
+    y="Sum",
+    color='Sum',
+    # color_continuous_scale='Bluered'
+    color_continuous_scale=["blue", "white", "red"]
+    )
+fig.update_layout(plot_bgcolor = 'white')
+fig.update_traces(marker_line_color='rgb(8,48,107)',
+                  marker_line_width=0.5, opacity=1)
+# fig.update_traces(marker={'size': 5, 'color': 'green'})
+
+
+# fig = px.scatter(
+#     magniperyear,
+#     x=magniperyear.index,
+#     y="Sum",
+#     color='Count',
+#     # trendline="lowess",
+#     # trendline_options=dict(frac=0.5)
+#     trendline="rolling",
+#     trendline_options=dict(window=3)
+#     )
+
 
 
 fig.show()
