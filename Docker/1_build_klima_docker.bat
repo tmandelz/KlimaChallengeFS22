eda95113253b64411674e7665b53bd54b017c2c3
Thomas Mandelz <tmandelz@hotmail.com>
2022-05-07 12:53:43 +0200

    added docker ignore and ports

--- Docker/1_build_klima_docker.bat
+++ Docker/1_build_klima_docker.bat
@@ -1,3 +1,5 @@
 cd C:\Users\Tom\OneDrive\Dokumente\Github\KlimaChallengeFS22
 docker build -f .\Docker\Dockerfile .  -t klimachallengefs22:heatwaves
+rem debian
+rem docker build -f ./Docker/Dockerfile .  -t klimachallengefs22:heatwaves
 pause
