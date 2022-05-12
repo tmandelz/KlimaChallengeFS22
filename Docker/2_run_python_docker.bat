01c0ff97012fec485c1cf439cb75ee65033ed6e1
Thomas Mandelz <tmandelz@hotmail.com>
2022-05-12 16:44:24 +0200

    add some docker commands

--- Docker/2_run_python_docker.bat
+++ Docker/2_run_python_docker.bat
@@ -1,6 +1,11 @@
 REM start python bash
 docker exec -it Python /bin/bash
 
-REM strace -p1 -s9999 -e write 
+
+
+rem docker ps
+rem docker cp France.csv ContainerID:/klima/Data/UnprocessedData/France.csv
+
 rem nohup python Datapipeline.py > output.log &
-rem nohup python test.py > output.log &
+rem ps aux
+REM strace -p1 -s9999 -e write 
