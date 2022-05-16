5a13e61850a47a04ee9fd80ed3dd7ad9e31a1da5
Thomas Mandelz <tmandelz@hotmail.com>
2022-05-16 13:35:31 +0200

    some more commands

--- Docker/3_run_postgres_docker.bat
+++ Docker/3_run_postgres_docker.bat
@@ -1,2 +1,3 @@
 REM start postgres docker file with psql
-docker exec -it postgres psql postgres://klima:orDtiURVtHUHwiQDeRCv@db:5432/klimachallengefs22
+docker exec -it postgres psql postgres://root:orDtiURVtHUHwiQDeRCv@db:5432/klimachallengefs22
+pg_dump klimachallengefs22 | gzip > /backups/klimachallengefs22.gz
--- Docker/4_run_gunicorn_webserver.bat
+++ Docker/4_run_gunicorn_webserver.bat
@@ -1,3 +1,3 @@
 REM start gunicorn webserver
-gunicorn DashboardServer:server --bind 172.28.1.5:8050 --timeout 360
+@REM gunicorn DashboardServer:server --bind 172.28.1.5:8050 --timeout 360
 nohup gunicorn DashboardServer:server --bind 172.28.1.5:8050 --timeout 360 > outputDash.log &
