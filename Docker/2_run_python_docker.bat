050d1041b61b1f4a67cb52ad8d2f83ed5cecaebe
Thomas Mandelz <tmandelz@hotmail.com>
2022-05-20 09:48:08 +0200

    Präsentationsstand 20.5 Server component

--- Docker/2_run_python_docker.bat
+++ Docker/2_run_python_docker.bat
@@ -4,7 +4,9 @@ docker exec -it Python /bin/bash
 
 
 rem docker ps
-rem docker cp France.csv ContainerID:/klima/Data/UnprocessedData/France.csv
+rem docker cp France.csv 4c9deee06a26:/klima/Data/UnprocessedData/France.csv
+rem docker cp Datapipeline.py 4c9deee06a26:/klima/Datapipeline.py
+rem docker cp DashboardServer.py 4c9deee06a26:/klima/DashboardServer.py
 
 rem nohup python Datapipeline.py > output.log &
 rem ps aux
